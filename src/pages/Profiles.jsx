067798505d5ebe04eb2299889fb8b0bd1e0e8c54
GP <gregpetropoulos@yahoo.com>
2022-03-17 06:01:35 -0400

    Built out the busuness logic for profile display and change user details and store into the firestore db. Included conditional classNames ran by the boolean state logic.

--- src/pages/Profiles.jsx
+++ src/pages/Profiles.jsx
@@ -1,18 +1,102 @@
-import{getAuth} from 'firebase/auth'
-import { useState, useEffect } from 'react';
 
+// Email is difficult to change since firestore uses it as authentication
+import { getAuth, updateProfile } from 'firebase/auth';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+// UPDATE THE FIRESTORE
+import { updateDoc, doc } from 'firebase/firestore';
+import { db } from '../config/firebase.config';
 
 const Profiles = () => {
-const [user,setUser]= useState (null)
-const auth =getAuth()
+  const auth = getAuth();
 
-useEffect(()=> {
-  console.log('useEffect check', auth.currentUser);
-  setUser(auth.currentUser);
+  // BUTTON TO ENABLE THE FORM
+  const [changeDetails, setChangeDetails] = useState(false);
 
-},[])
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email
+  });
 
-  return user? <h1>{user.displayName}</h1>:'Not Logged In'
+  const { name, email } = formData;
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate('/');
+  };
+  const onSubmit = async () => {
+    try {
+      // check the current users name is not the same name entered the it will be updated and stored in the firestore db
+      if (auth.currentUser.displayName !== name) {
+        // *UPDATE NAME 
+        // takes in 2 arguments current user and the object to update, will return a promise
+        await updateProfile(auth.currentUser, {
+          displayName: name
+        });
+        // * STORE IN DB
+        // find db with users collection with uid of current user and update the name
+        const userRef= doc( db, 'users', auth.currentUser.uid)
+        await updateDoc(userRef, {name})
+      }
+    } catch (error) {
+      toast.error('Could not update profile details')
+    }
+  };
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }));
+  };
+  return (
+    <div className='profile'>
+      <header className='profileHeader'>
+        <p className='pageHeader'>My Profile</p>
+        <button className='logOut' type='button' onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+      <main>
+        <div className='profileDetailsHeader'>
+          <p className='profileDetailsText'>Personal Details</p>
+          <p
+            className='changePersonalDetails'
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}>
+            {changeDetails ? 'done' : 'change'}
+          </p>
+        </div>
+        ˜
+        <div className='profileCard'>
+          <form className=''>
+            <input
+              type='text'
+              id='name'
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type='text'
+              id='email'
+              className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Profiles;
