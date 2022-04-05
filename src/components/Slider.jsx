25832947c2e7de70716334ad6aa95e3fb588be4f
GP <gregpetropoulos@yahoo.com>
2022-04-05 06:21:36 -0400

    Created an slider component and brought it into the Offer parent component as a slider at the top of the offer page.

---
+++ src/components/Slider.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { collection, getDocs, query, orderBy, limit } from 'firebase/firestore';
+import { db } from '../config/firebase.config';
+import Spinner from './Spinner';
+
+// Swiper
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+const Slider = () => {
+  const [loading, setLoading] = useState(true);
+  const [listings, setListings] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      const listingRef = collection(db, 'listings');
+      const q = query(listingRef, orderBy('timestamp', 'desc'), limit(5));
+      const querySnap = await getDocs(q);
+
+      // initialize array
+      let listings = [];
+
+      // loop through and push onto listings array `id` and `data`
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data()
+        });
+      });
+      //   update state
+      setListings(listings);
+      setLoading(false);
+    };
+    fetchListing();
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
+  return (
+    listings && (
+      <>
+        <p className='exploreHeading'>Recommended</p>
+        <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+          {listings.map(({ data, id }) => (
+            <SwiperSlide
+              key={id}
+              onClick={() => navigate(`/category/${data.type}/${id}`)}>
+              <div
+                style={{
+                  background: `url(${data.imgUrls[0]})center no-repeat`,
+                  backgroundSize: 'cover'
+                }}
+                className='swiperSlideDiv'>
+                <p className='swiperSlideText'>{data.name}</p>
+                <p className='swiperSlidePrice'>
+                  {data.discountedPrice ?? data.regularPrice}{' '}
+                  {data.type === 'rent' && '/month'}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    )
+  );
+};
+
+export default Slider;
--- src/pages/Explorer.jsx
+++ src/pages/Explorer.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg';
 import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg';
+import Slider from '../components/Slider';
 
 const Explorer = () => {
   return (
@@ -9,8 +10,7 @@ const Explorer = () => {
         <p className='pageHeader'>Explore</p>
       </header>
       <main>
-        {/* Slider */}
-
+        <Slider />
         <p className='exploreCategoryHeading'>Categories</p>
 
         <div className='exploreCategories'>
@@ -20,7 +20,7 @@ const Explorer = () => {
               alt='rent'
               className='exploreCategoryImg'
             />
-            <p className="exploreCategoryName">Places For Rent</p>
+            <p className='exploreCategoryName'>Places For Rent</p>
           </Link>
           <Link to='/category/sale'>
             <img
@@ -28,7 +28,7 @@ const Explorer = () => {
               alt='sale'
               className='exploreCategoryImg'
             />
-            <p className="exploreCategoryName">Places For Sale</p>
+            <p className='exploreCategoryName'>Places For Sale</p>
           </Link>
         </div>
       </main>
